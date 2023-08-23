fc4f0d668c64e3d3dffe82dee7dda521cd781a2b
Sam <kulkarni.samarth@yahoo.com>
2023-08-23 10:37:09 -0400

    Added Initial Files
    Updated readme, added meeting 2 activity code

---
+++ Meeting_2_Activity.py
@@ -0,0 +1,55 @@
+############################################################
+# CMPSC 442: Meeting 2 Activity (Python #1)
+############################################################
+############################################################
+# Section 1: Dictionaries
+#
+# First illustration of the utility of zip(); also very useful
+# with higher order functions
+#
+############################################################
+# The function assemble_dict should return a dictionary using
+# the ordered list 'dictkeys' as the dictionary keys and the
+# ordered list 'dictvalues' as the corresponding values. If the
+# two lists are not the same length, the longer list is truncated
+# to an equal length by dropping the excess items from the end.
+# Note that assemble_dict can be defined in one line using the python
+# function zip(), and that append_dict can use a dictionary method called
+# update()
+
+def assemble_dict(dictkeys, dictvalues):
+    pass
+
+
+def append_dict(dict1, dict2):
+    pass
+
+
+############################################################
+# Section 2: Sequence slicing (making copies)
+#
+# Recall that the slice parameters take on sensible default
+# values when omitted. In some cases, it may be necessary to use
+# the optional third parameter to specify a step size.
+############################################################
+# The function copy(mylist, add) should return a tuple (mylist, newlist)
+# where newlist is a shallow copy of the original mylist and where add
+# is appended to mylist
+def copy(mylist, new):
+    pass
+
+
+# The function all_but_last(seq) should return a new sequence
+# containing all but the last element of the input sequence.
+# If the input sequence is empty, a new empty sequence of the same type
+# should be returned.
+def all_but_last(seq):
+    pass
+
+
+# The function every_other(seq) should returns a new sequence
+# containing every other element of the input sequence, starting
+# with the first. Hint: This function can be written in one
+# line using the optional third parameter of the slice notation.
+def every_other(seq):
+    pass
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CMPSC442-CodeRepo
+A repository to store code and projects for CMPSC 442 at Penn State University. Taken during the fall 2023 semester, under Professor Rebecca Passonneau.
+
+For notes on the course, see the [notion page](https://achieved-damselfly-0d0.notion.site/CMPSC-442-AI-119d17b0d91a4df6953458ad8c46a73c?pvs=4).
+## Projects List
+- Meeting 2 Activity
+  - Dictionaries
+  - Sequence Slicing
+
+
